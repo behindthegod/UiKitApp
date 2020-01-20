f4e1bc18f9d4800af700a5e1a0895db3bc91324d
57k <57k@mail.ru>
2020-01-20 16:43:33 +0300

    Initial Commit

---
+++ UiKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UiKitApp
+//
+//  Created by Илья Карлюк on 20.01.2020.
+//  Copyright © 2020 Илья Карлюк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
